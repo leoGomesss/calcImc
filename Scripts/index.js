13973c29cfe170d1586e2ff70876a58e9e13fa48
Leomar Gomes <126305987+leoGomesss@users.noreply.github.com>
2026-08-23 01:58:42 -0300

    230826
    js, css

--- Scripts/index.js
+++ Scripts/index.js
@@ -3,18 +3,44 @@ const btnLimpar = document.querySelector("#limpar");
 const inputAltura = document.querySelector("#ialtura"); 
 const inputPeso = document.querySelector("#ipeso");
 const pResultado = document.querySelector("#resultado");
+const container = document.querySelector(".container"); // pega a div
+const containerCalc = document.querySelector("#containerCalc"); // pega a div
 /*const pResultadoPeso = document.querySelector("#resultadoPeso");*/
+const btnVoltar = document.querySelector("#btnVoltar"); // novo botão
+
+// CORRIGIDO - pega as divs certas
+
+const containerImc = document.querySelector("#containerImc"); // a classificação
 
 function acaoBotao() {
     const alturaDigitada = parseFloat(inputAltura.value.replace(',','.'));
     const pesoDigitado = parseFloat(inputPeso.value.replace(',','.'));
 
-    const imc = pesoDigitado/(alturaDigitada*alturaDigitada);
+    const imc = parseFloat(pesoDigitado/(alturaDigitada*alturaDigitada));
 
-    pResultado.innerHTML = "Seu IMC é: " + imc.toFixed(2) + " kg/m²";
-    /*pResultadoPeso.innerHTML = "Seu Peso é: " + pesoDigitado + "kg";*/
+    if (!alturaDigitada || !pesoDigitado) {
+        pResultado.innerText = "Digite altura e peso!";
+        return;
+    }else {
+
+        if(imc < 18.5){
+            // Esconde a calculadora
+            containerCalc.style.display = "none";
+
+            // Mostra a classificação
+            containerImc.style.display = "block";
+            containerImc.style.opacity = "1";
+
+            pResultado.innerHTML = "Seu IMC é: " + imc.toFixed(2) + " kg/m² <br> <br> Classificação: Magreza";
+        } else {
+            // Se IMC for maior que 18, mantém calculadora aparecendo
+            containerCalc.style.display = "block";
+            containerImc.style.display = "none";
+    }
+    }    
 
 }
+
 function limparTudo() { // 2. Cria a função de limpar
     inputAltura.value = ""; // apaga o input altura
     inputPeso.value = "";   // apaga o input peso
@@ -22,7 +48,21 @@ function limparTudo() { // 2. Cria a função de limpar
     inputAltura.focus(); // opcional: volta o cursor pra altura
 }
 
+function voltarParaCalculadora() {
+    // Mostra a calculadora de novo
+    containerCalc.style.display = "flex";
+    
+    // Esconde a tela de resultado
+    containerImc.style.display = "none";
+    containerImc.style.opacity = "0";
+
+    // Opcional: limpa os campos
+    // inputAltura.value = "";
+    // inputPeso.value = "";
+}
+
 btnBotao.addEventListener("click",acaoBotao);
 btnLimpar.addEventListener("click", limparTudo);
+btnVoltar.addEventListener("click", voltarParaCalculadora);
 
 
